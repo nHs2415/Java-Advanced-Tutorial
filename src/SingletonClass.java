4c2fb57f06b9b63c445bedc97044e65565397a20
nHs2415 <nuwanihansi92@gmail.com>
2023-09-28 19:12:13 -0700

    initial commit

---
+++ src/SingletonClass.java
@@ -0,0 +1,17 @@
+public final class SingletonClass {
+
+    public static final int Max_Age=18;
+
+    private int index;
+    private byte grade;
+    private String name;
+
+    public SingletonClass(){
+
+    }
+    public SingletonClass(String name,byte grade){
+        this.grade=grade;
+        this.name=name;
+    }
+
+}
